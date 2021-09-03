bffb298d441bb248a1033e5d554d9683f8790835
r3corp <baudecoisas@gmail.com>
2021-09-03 11:34:04 -0300

    Code from interview

---
+++ oracle/cmake-build-debug/CMakeFiles/oracle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oracle.dir/main.cpp.o"
+  "oracle"
+  "oracle.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oracle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
